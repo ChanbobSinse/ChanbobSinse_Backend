62e77b7766924c9cddaf33e91af8a6d57b93af12
akat32 <baduk32127@gmail.com>
2018-02-15 22:10:41 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+module.exports = (router, Users, passport, rndString) =>{
+  var users = require('./models/users')(router, Users, passport);
+  var auth = require('./models/auth')(router, Users, passport);
+
+  router.use('/auth', auth);
+  router.use('/users', users);
+
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  })
+
+  return router;
+};
---
+++ routes/models/users.js
@@ -0,0 +1,40 @@
+module.exports = (router, Users)=>{
+  router.use(isAuth);
+
+  router.get('/', async (req, res)=>{
+    if(req.user.is_admin){
+      var users = await Users.find();
+      res.status(200).json(users);
+    }else res.sendStatus(404);
+  })
+
+  .get('/:id', (req, res)=>{
+
+  })
+
+  .put('/:id', (req, res)=>{
+
+  })
+
+  .patch('/:id', (req, res)=>{
+
+  })
+
+  .delete('/', async (req, res)=>{
+    if(req.user.is_admin){
+      var user = req.body.id;
+      var result = await Users.remove(user);
+      if(result) res.status(200).json({message: "success"});
+      else res.status(500).json({message: "server err"});
+    }else{
+      var result = await Users.remove(req.user);
+      if(result) {
+        req.logout();
+        res.status(200).json({message: "success"});
+      }
+      else res.status(500).json({message: "server err"});
+    }
+  })
+
+  return router;
+}
